7e16005d60995bf3b871f8fc6a864ff25d86a0bb
andr422 <68136555+andr422@users.noreply.github.com>
2021-03-14 23:49:15 -0700

    Add files via upload
    Solution for problem one.

---
+++ problemOne.java
@@ -0,0 +1,21 @@
+public class problemOne {
+    public static void main(String[] args) {
+        System.out.println(findSum());
+    }
+
+
+    public static long findSum() {
+        int result = 0;
+        for (int i = 0; i < 1000; i++) {
+            if(checkMultiple(i)) {
+                result += i;
+            }
+        }
+        return result;
+    }
+
+
+    public static boolean checkMultiple(int i) {
+        return ((i%3==0)||(i%5==0));
+    }
+}
